f3e9a2308e425d37ea257b925543aa75a71ba2d5
hopealarcon <hopealarcon@users.noreply.github.com>
2026-03-18 20:49:04 -0500

    Fix CORS to allow all origins

--- backend/api.py
+++ backend/api.py
@@ -18,11 +18,10 @@ load_dotenv()
 
 app = FastAPI()
 
-ALLOWED_ORIGINS = os.environ.get("ALLOWED_ORIGINS", "*").split(",")
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=ALLOWED_ORIGINS,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
